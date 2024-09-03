1f07e16f27e8b3110adcaa920910eed1ccd6f20d
Хайрулин Сергей <hiroolin@hotmail.com>
2024-09-03 08:34:26 +0500

    1

--- ParallelReading/Program.cs
+++ ParallelReading/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Diagnostics;
+using System.Linq;
+
 internal class Program
 {
     private static string _folderPath = @"..\..\..\Samples"; // Укажите путь к вашей папке
@@ -11,23 +13,38 @@ internal class Program
 
     private static async Task Main(string[] args)
     {
-        // 1: Прочитать 3 файла параллельно и вычислить количество пробелов
-        var spaceCounts = await Task.WhenAll(_files.Select(file => CountSpacesInFileAsync(file)));
-        // var spaceCounts = Client.GetClients().AsParallel().Select(async file => await CountSpacesInFileAsync(file));
-        for (int i = 0; i < _files.Length; i++)
+        try
         {
-            Console.WriteLine($"Количество пробелов в {_files[i]}: {spaceCounts[i]}");
+            // 1: Прочитать 3 файла параллельно и вычислить количество пробелов
+            int[] spaceCounts = await CountSpacesInFilesAsync(_files);
+            for (int i = 0; i < _files.Length; i++)
+                Console.WriteLine($"Количество пробелов в {_files[i]}: {spaceCounts[i]}");
+
+            // 2: Прочитать все файлы в папке и вычислить количество пробелов
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            string[] folder_files = Directory.GetFiles(_folderPath);
+            spaceCounts = await CountSpacesInFilesAsync(folder_files);
+            int totalSpaceCount = spaceCounts.Sum();
+            stopwatch.Stop();
+            Console.WriteLine($"Общее количество пробелов в папке: {totalSpaceCount}");
+            Console.WriteLine($"Время выполнения: {stopwatch.ElapsedMilliseconds} мс.");
+
         }
-
-        // 2: Прочитать все файлы в папке и вычислить количество пробелов
-        var stopwatch = Stopwatch.StartNew();
-        var totalSpaceCount = await CountSpacesInFolderAsync(_folderPath);
-        stopwatch.Stop();
-
-        Console.WriteLine($"Общее количество пробелов в папке: {totalSpaceCount}");
-        Console.WriteLine($"Время выполнения: {stopwatch.ElapsedMilliseconds} мс");
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка в просессе выполения: {ex.Message}");
+        }
         Console.Read();
     }
+
+    // Количество пробелов в нескольких файлах (files)
+    // Выполняется параллельно
+    static async Task<int[]> CountSpacesInFilesAsync(IEnumerable<string> files)
+    {
+        return await Task.WhenAll(files.Select(file => CountSpacesInFileAsync(file)));
+    }
+
+    // Количество пробелов в одном файле (filePath)
     static async Task<int> CountSpacesInFileAsync(string filePath)
     {
         using (var reader = new StreamReader(filePath))
@@ -35,13 +52,5 @@ internal class Program
             string content = await reader.ReadToEndAsync();
             return content.Count(c => c == ' ');
         }
-    }
-
-    static async Task<int> CountSpacesInFolderAsync(string folderPath)
-    {
-        var files = Directory.GetFiles(folderPath);
-        var spaceCounts = await Task.WhenAll(files.Select(file => CountSpacesInFileAsync(file)));
-         // var spaceCounts = Client.GetClients().AsParallel().Select(async file => await CountSpacesInFileAsync(file));
-       return spaceCounts.Sum();
     }
 }
